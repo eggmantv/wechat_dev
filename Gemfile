8b4b01c432755b9559b01655c12103598502507a
Lei Lee <mytake6@gmail.com>
2017-03-16 16:15:19 +0800

    message auto reply

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'wechat-gate', '>= 0.1.4'
+gem 'nokogiri'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.2'
--- app/controllers/wechat/push_controller.rb
+++ app/controllers/wechat/push_controller.rb
@@ -13,6 +13,28 @@ module Wechat
       body = request.body.read
       Rails.logger.info "WECHAT: #{body}"
 
+      doc = Nokogiri::XML(body)
+      user_open_id = doc.search("FromUserName").text
+
+      event = doc.search("Event").text
+      event_key = doc.search("EventKey").text
+
+      msg_type = doc.search("MsgType").text
+      content = doc.search("Content").text
+      case msg_type.to_sym
+      when :text
+        render xml: wechat_gate_config.message_body(:text, user_open_id, "我们收到了: #{content}")
+        return
+      when :event
+        case event.to_sym
+        when :subscribe
+          render xml: wechat_gate_config.message_body(:text, user_open_id, "欢迎关注蛋人网!")
+          return
+        end
+      else
+        Rails.logger.info "WECHAT: push: unknown msg_type #{msg_type}"
+      end
+
       render plain: 'success'
     end
 
